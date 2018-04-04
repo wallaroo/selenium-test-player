58ed467640216fb5b852018e6ac217f9afc4cefa
wallaroo <2890832+wallaroo@users.noreply.github.com>
2018-04-04 17:34:18 +0200

    feat: confluence publish also images

--- packages/selenium-test-player-report-confluence/src/publish.ts
+++ packages/selenium-test-player-report-confluence/src/publish.ts
@@ -1,16 +1,17 @@
 #!/usr/bin/env node
 import * as commander from "commander"
-import { readdir, readFile, stat } from "fs";
+import { readdir, readFile, stat, createReadStream } from "fs";
+import { join } from "path";
 import fetch from "node-fetch";
 import { promisify } from "util";
+import * as FormData from "form-data";
 const btoa = require("btoa");
 
 const fileRead = promisify(readFile);
 const stats = promisify(stat);
 const readDir = promisify(readdir);
-
 commander
-  .usage("[options] <pageid> <contentfile>")
+  .usage("[options] <pageid> <contentdir>")
   .option(
     "-c, --credentials [credentials]", "Confluence credentials"
   )
@@ -19,11 +20,11 @@ commander
   )
   .parse(process.argv);
 
-const [ pageid, contentFile ] = commander.args;
+const [ pageid, contentdir ] = commander.args;
 const {credentials, url} = commander;
 
 console.log("PAGE ID: ", pageid);
-console.log("Content File: ", contentFile);
+console.log("Content Dir: ", contentdir);
 console.log("URL: ", url);
 (async () => {
   const response = await fetch(`${url}/rest/api/content/${pageid}`, {
@@ -31,10 +32,59 @@ console.log("URL: ", url);
       "Authorization": "Basic " + btoa(credentials)
     }
   });
+  // upload images
+  for(const file of await readDir(join(contentdir,"__img__"))){
+    const existsRes = await fetch(`${url}/rest/api/content/${pageid}/child/attachment?filename=${file}`,{
+      headers: {
+        "Authorization": "Basic " + btoa(credentials)
+      }
+    });
+    const fileStats = await stats(join(contentdir, "__img__", file));
+    const fileContent = createReadStream(join(contentdir, "__img__", file));
+    const formData = new FormData();
+    formData.append("file", fileContent,{
+      filename:file,
+      contentType:"image/png",
+      knownLength: fileStats.size
+    });
+    formData.append("minorEdit","true");
+    formData.append("hidden","true");
+    let uploadres;
+    const existingFile = await existsRes.json();
+    if (existingFile.size){
+      const fileId = existingFile.results[0].id;
+      uploadres = await fetch(`${url}/rest/api/content/${pageid}/child/attachment/${fileId}/data`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Authorization": "Basic " + btoa(credentials),
+          "X-Atlassian-Token": "nocheck",
+          ...formData.getHeaders()
+        }
+      });
+    }else {
+      uploadres = await fetch(`${url}/rest/api/content/${pageid}/child/attachment?allowDuplicated=true`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          "Authorization": "Basic " + btoa(credentials),
+          "X-Atlassian-Token": "nocheck",
+          ...formData.getHeaders()
+        }
+      });
+    }
+    if (uploadres.ok){
+      console.log(file, "UPLOADED");
+    }else{
+      console.error(`${file} UPLOAD FAILED`);
+      console.error(await uploadres.text())
+    }
+  }//end upload images
+
   if (response.ok) {
     const jsonres = await response.json();
     const newVersion = jsonres.version.number + 1;
-    const content = await fileRead(contentFile, "utf8");
+    const content = await fileRead(join(contentdir,"report.cwk"), "utf8");
     const body = JSON.stringify({
         "id": pageid,
         "type": "page",
@@ -47,10 +97,11 @@ console.log("URL: ", url);
           }
         },
         "version": {
-          "number": newVersion
+          "number": newVersion,
+          "minorEdit":true,
+          "hidden":true
         }
       });
-    //console.log("BODY", body);
     const putResponse = await fetch(`${url}/rest/api/content/${pageid}`, {
       method: "PUT",
       headers: {
@@ -70,10 +121,3 @@ console.log("URL: ", url);
     console.error(await response.text())
   }
 })();
-
-/*
-{"id":"3604482","type":"page",
-"title":"new page","space":{"key":"TST"},"body":{"storage":{
-"value":"<p>This is the updated text for the new page</p>","representation":"storage"}},
-"version":{"number":2}}
- */
